5f1191f1cae74dab1458c9f3553d714f9f4eaff8
Voggo <nielsviggostark@gmail.com>
2021-11-23 10:21:54 +0100

    1

--- src/sum.c
+++ src/sum.c
@@ -2,12 +2,9 @@
 
 int sum(int a[], int n)
 {
-    int result;
-
     if (n == 0){
         return 0;
     } else {
-        result = result + a[n-1];
-        return result + sum(a, n-1);;
+        return  a[n-1] + sum(a, n-1);
     }
 }
